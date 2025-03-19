0d4492a23f34c989b05b1d852826de47e8a3115b
BjRkaZs <bajuszrekazsofi@ktch.hu>
2025-03-19 18:12:19 +0100

    fordit

--- MedicineBackend/resources/views/emails/restock-reminder.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <h2>Medicine Restock Reminder</h2>
-    <p>Your medicine {{ $medicineName }} is running low!</p>
-    <p>Current stock: {{ $stock }}</p>
-    <p>Please remember to restock by: {{ $restockDate }}</p>
-</body>
-</html>
